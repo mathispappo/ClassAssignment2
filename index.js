a3e1c789a12a280bdd96afcad80f4486729b23ba
Mathis Pappo <43952106+mathispappo@users.noreply.github.com>
2022-10-10 19:45:57 -0700

    Created Login File

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+})
+
 app.listen(port, () => {
     console.log(`Example app lostening on port ${port}`);
 })
